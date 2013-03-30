c14b2d7db420fd647380868be9500fadc586dfa9
Paul Walger <metaxy@walger.name>
2013-03-30 20:41:53 +0100

    install cronjob

--- install.sql
+++ install.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS `civicrm_payflowpro_recur` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 UPDATE `civicrm_payment_processor_type` SET is_recur = '1' WHERE name = 'PayflowPro';
+
+INSERT INTO `civicrm_job` (`id`, `domain_id`, `run_frequency`, `last_run`, `name`, `description`, `api_prefix`, `api_entity`, `api_action`, `parameters`, `is_active`) VALUES
+(15, 1, 'Daily', NULL, 'PayflowPro', '', 'civicrm_api3', 'Job', 'payflowpro', '', 1);
